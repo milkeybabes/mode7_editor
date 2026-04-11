a01ff4d1434d71354c08ed3e8591f440235d5ff3
Michael Archer <42204126+milkeybabes@users.noreply.github.com>
2026-04-11 15:19:16 +0800

    Add files via upload
    Demo example of how to use for a game project.

---
+++ Examples/StreetRacer_Maps.bat
@@ -0,0 +1,12 @@
+python ..\mode7_editor.py Biff
+python ..\mode7_editor.py Frank
+python ..\mode7_editor.py Helmut
+python ..\mode7_editor.py Hodja
+python ..\mode7_editor.py Raphael
+python ..\mode7_editor.py Sumo_San
+python ..\mode7_editor.py Surf
+python ..\mode7_editor.py Suzulu
+python ..\mode7_editor.py Indoor_Pitch
+python ..\mode7_editor.py Outdoor_Pitch
+python ..\mode7_editor.py Rumble
+
